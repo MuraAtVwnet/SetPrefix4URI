9720c1af7936750adfd6526908f6531ba0a5be7c
Syuichi Murashima <mura-github@vwnet.jp>
2020-08-29 10:49:29 +0900

    1st commit

---
+++ Install4Win.ps1
@@ -0,0 +1,14 @@
+﻿#################################
+# Create Shortcut
+#################################
+$DesktopPath = Join-Path ($env:HOMEDRIVE) ($env:HOMEPATH)
+$DesktopPath = Join-Path $DesktopPath "Desktop"
+
+$ShortcutPath = Join-Path $DesktopPath "SetPrifix4URI.lnk"
+$TergetBatchPath = Join-Path $PSScriptRoot "SetPrifix4URI.bat"
+
+$WsShell = New-Object -ComObject WScript.Shell
+$Shortcut = $WsShell.CreateShortcut($ShortcutPath)
+$Shortcut.TargetPath = $TergetBatchPath
+# $Shortcut.IconLocation = "C:\tools\procexp.exe"
+$Shortcut.Save()
---
+++ SetPrefix4URI.ps1
@@ -0,0 +1,27 @@
+﻿##############################################
+# Set Prefix for URI
+##############################################
+
+# Prefix
+$Prifix = 'Prifix'
+
+[array]$URI = Get-Clipboard
+
+Write-Output "Input URI : $URI"
+
+# Reject Facebook Prifix
+$URI[$URI.Count -1] = $URI[$URI.Count -1] -replace "\?fbclid=.+", ""
+
+# Add last /
+if($URI[$URI.Count -1][$URI[$URI.Count -1].Length -1] -ne "/"){
+	$URI[$URI.Count -1] += "/"
+}
+
+# Add Prifix
+$URI[$URI.Count -1] += $Prifix
+
+Write-Output "Output URI : $URI"
+
+$URI | Set-Clipboard
+
+Read-Host "Please Enter"
